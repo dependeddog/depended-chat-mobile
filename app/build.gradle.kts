80ad7aa6ec6afe14c3875f10451d3215496022d7
Honfi555 <kasanindaniil@gmail.com>
2026-03-17 13:58:09 +1000

    feature (app): update conf files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
-        buildConfigField("String", "BASE_URL", '"http://10.0.2.2:8000/"')
-        buildConfigField("String", "WS_BASE_URL", '"ws://10.0.2.2:8000"')
+        buildConfigField("String", "BASE_URL", "http://localhost:8000/")
+        buildConfigField("String", "WS_BASE_URL", "ws://localhhost:8000")
     }
 
     buildTypes {
