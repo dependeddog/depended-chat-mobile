df93681deb1dfd89b3547bbc5318e73faaad9b12
dependeddog <dependeddog@yandex.ru>
2026-04-01 01:38:32 +1000

    Remove Firebase Analytics to avoid Kotlin metadata mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,6 +98,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
-    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-messaging")
 }
